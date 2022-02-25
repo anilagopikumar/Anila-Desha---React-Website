a10cd63a0677ee2de8868b2dcc11ab0472ac7a50
Anila Gopikumar <anila.desha094@gmail.com>
2022-02-25 13:13:23 +0530

    modified

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -12,20 +12,19 @@ return (
 <div className='portfolio__content'>
 <article className='portfolio__details'>
 <div>
-<h4>Software Developer - Prolifics Corporation - Hyderabad , India</h4>
-<small className='text-light'>July 2021 - Present</small>
+<h4>Software Developer - Prolifics Corporation - Hyderabad , India - Present</h4>
 </div>
 </article>
 <article className='portfolio__details'>
 <div>
 <h4>Full Stack Intern - Edvenswa Tech Pvt ltd - Hyderabad , India</h4>
-<small className='text-light'>Apr 2021 - Jun 2021</small>
+
 </div>
 </article>
 <article className='portfolio__details'>
 <div>
 <h4>Systems Engineer Intern - Clinch Infotech - Hyderabad , India</h4>
-<small className='text-light'>Jan 2021 - Mar 2021</small>
+
 </div>
 </article>
 </div>
@@ -35,20 +34,17 @@ return (
 <div className='portfolio__content'>
 <article className='portfolio__details'>
 <div>
-<h4>B.E - IT , Snist - Aff JNTU - Hyderabad , India</h4>
-<small className='text-light'>May 2018 , 7.6 CGPA</small>
+<h4>B.Tech/B.E - IT , Snist - Aff JNTU - Hyderabad , India</h4>
 </div>
 </article>
 <article className='portfolio__details'>
 <div>
 <h4>Secondary Education , BIE - Hyderabad , India</h4>
-<small className='text-light'>May 2014 , 9.2 CGPA</small>
 </div>
 </article>
 <article className='portfolio__details'>
 <div>
 <h4>10TH Standard , BSET - Hyderabad , India</h4>
-<small className='text-light'>May 2012 , 9.5 CGPA</small>
 </div>
 </article>
 </div>
